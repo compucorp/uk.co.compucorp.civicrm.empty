67b73eaccc814e4f1f2bcc18da93b22d6d17e8ca
Davi Alexandre <davi@davialexandre.com.br>
2018-10-05 17:01:24 -0300

    Add version 1.1

---
+++ CRM/Empty/Upgrader.php
@@ -0,0 +1,13 @@
+<?php
+use CRM_Empty_ExtensionUtil as E;
+
+/**
+ * Collection of upgrade steps.
+ */
+class CRM_Empty_Upgrader extends CRM_Empty_Upgrader_Base {
+    
+    public function upgrade_1001() {
+        return TRUE;
+    }
+
+}
